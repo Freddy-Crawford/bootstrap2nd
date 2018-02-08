567c4ff18e333431e83b01c526630ad71706d897
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-07 22:29:39 -0700

    tried bootstrap challenge again

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>bootstrap challenge</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+	</head>
+	<body>
+		<nav class="navbar navbar-sm navbar-light bg-light">
+			<a class="navbar-brand" href="#">Title</a>
+						<a class="nav-link" href="#">Link</a>
+						<a class="nav-link" href="#">link</a>
+						<a class="nav-link" href="#">link</a>
+			</div>
+		</nav>
+
+		<div class="jumbotron">
+				<h1> Hello, world</h1>
+						<p>Curabitizzle izzle izzle get down get down nisi boom shackalack funky fresh. Suspendisse Sle.</p>
+		</div>
+
+
+		<div class="jumbotron rounded">
+			<div class="row">
+				<div class="span4">
+					<img style="float:left" src="mobile2.png" height="100" width="100"/>
+					<p>Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+
+				Lorem rizzle dolizzle crackalackin shit, shizznit adipiscing crazy. Nullam dawg velizzle, black volutpizzle, i'm in the
+
+					shizzle shit, gravida vizzle, doggy. Pellentesque pimpin' tortor. Dawg eros. Fusce izzle dolizzle fizzle doggy tempizzle
+
+					dang. Maurizzle shizzle my nizzle crocodizzle nibh et turpis. Vestibulum izzle fizzle. Pellentesque yippiyo rhoncizzle nisi.
+
+					In hizzle i saw beyonces tizzles and my pizzle went crizzle platea dictumst. Donec dapibizzle. The bizzle tellus brizzle,
+
+					pretium shizznit, da bomb ma nizzle, eleifend vitae, sure. Rizzle suscipit. Nizzle gangsta velit sizzle purizzle.</p>
+				</div>
+			</div>
+		</div>
+
+
+
+<div class="container">
+
+		<div class="row">
+
+					<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+							<img class="card-img-top rounded-circle p-1" src="mobile2.png" width="70" height="70" alt="card-cap">
+
+											<div class="card-block border rounded">
+
+									<p class="card-text">Lorem rizzle dolizzle crackalackin shit, shizznit  Dawg eros. Fusce izzle </p>
+
+							<div class="card-block bg-light border rounded m-2 p-2">
+
+						</div>
+
+			</div>
+
+
+			</div>
+
+
+			<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+				<img class="card-img-top rounded-circle p-1" src="mobile2.png" width="70" height="70" alt="card cap">
+
+				<div class="card-block border rounded">
+
+					<p class="card-text">Lorem rizzle dolizzle crackalackin shit, shizznit  Dawg eros. Fusce izzle </p>
+
+					<div class="card-block bg-light border rounded m-2 p-2">
+
+					</div>
+
+				</div>
+
+
+			</div>
+
+
+
+			<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+				<img class="card-img-top rounded-circle p-1" src="mobile2.png" width="70" height="70" alt="card cap">
+
+				<div class="card-block border rounded">
+
+					<p class=>Lorem rizzle dolizzle crackalackin shit, shizznit  Dawg eros. Fusce izzle </p>
+
+					<div class="card-block bg-light border rounded m-2 p-2">
+
+					</div>
+
+				</div>
+
+
+			</div>
+
+
+
+			<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+				<img class="card-img-top rounded-circle p-1" src="mobile2.png" width="70" height="70" alt="card cap">
+
+				<div class="card-block border rounded">
+
+					<p class="card-text">Lorem rizzle dolizzle crackalackin shit, shizznit  Dawg eros. Fusce izzle </p>
+
+					<div class="card-block bg-light border rounded m-2 p-2">
+
+					</div>
+
+				</div>
+
+
+			</div>
+
+
+		</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+</html>
